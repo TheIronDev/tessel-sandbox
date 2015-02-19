0b87f13b704566fb5a35ef77f4ec7707ed778056
Tyler Stark <theirondeveloper@gmail.com>
2015-02-19 00:08:19 -0800

    Fixing the file route... I think

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -26,7 +26,7 @@ router.post('/', function(req, res, next) {
 		{
 			public_id: req.body.title
 		});
-	fs.createReadStream(req.files.image.path, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
+	fs.createReadStream(req.body.image, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
 });
 
 module.exports = router;

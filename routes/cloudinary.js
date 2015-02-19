d65f04194b9200e49a55571f42f7df21a202f0c6
Tyler Stark <theirondeveloper@gmail.com>
2015-02-19 00:19:54 -0800

    Simplify the expected request

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -15,18 +15,9 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
 	var stream = cloudinary.uploader.upload_stream(function(result) {
 			console.log(result);
-			res.send('Done:<br/> <img src="' + result.url + '"/><br/>' +
-			cloudinary.image(result.public_id, {
-				format: "jpg",
-				width: 480,
-				height: 640,
-				crop: "fill"
-			}));
-		},
-		{
-			public_id: req.body.title
 		});
-	fs.createReadStream(req.body.image, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
+
+	fs.createReadStream(req.body).pipe(stream);
 });
 
 module.exports = router;

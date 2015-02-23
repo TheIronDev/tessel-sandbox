22bde7ec44429589b36fa526e3833b6eb2ad8428
Tyler Stark <theirondeveloper@gmail.com>
2015-02-22 20:50:06 -0800

    I wasn't listening to the stream on...

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -14,12 +14,22 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 	console.log('Because I am submitting an image (and nothing else), req.body should be the image buffer, I think.');
-	console.log(req.body);
-	var stream = cloudinary.uploader.upload_stream(function(result) {
-			console.log(result);
-		});
+	console.log('Trying stream.on("open") now.... makes sense why it wasn't working before.);
 
-	fs.createReadStream(req.body).pipe(stream);
+	var readStream = fs.createReadStream(req.body);
+
+	// This will wait until we know the readable stream is actually valid before piping
+	readStream.on('open', function () {
+		// This just pipes the read stream to the response object (which goes to the client)
+		readStream.pipe(cloudinary.uploader.upload_stream(function(result) {
+			console.log(result);
+		}));
+	});
+
+	// This catches any errors that happen while creating the readable stream (usually invalid names)
+	readStream.on('error', function(err) {
+		res.end(err);
+	});
 });
 
 module.exports = router;
